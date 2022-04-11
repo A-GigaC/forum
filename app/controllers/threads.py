e1330a25d35a7b7dd25d86b84637f8687733f26d
PFE <nporozenko@gmail.com>
2022-04-11 22:29:49 +0700

    day4.1

--- app/controllers/threads.py
+++ app/controllers/threads.py
@@ -1,13 +1,22 @@
 from aiohttp import web
 from models.thread import create_thread
+import json
+from jsonschema import validate
+
+schema = {
+    "type" : "object",
+    "properties" : {
+        "name" : {"type" : "string"},
+    },
+}
 
 routes = web.RouteTableDef()
 
 @routes.post('/api/threads/')
 async def post_thread(request):
-    name = request.text()
-    # create_thread(name)
+    name = await request.json()
     print(name)
-    
+    validate(instance=name, schema=schema)
+    await create_thread(name)
     return web.Response()
 
--- app/main.py
+++ app/main.py
@@ -4,11 +4,13 @@ app = web.Application()
 
 from controllers.threads import routes
 
-app.add_routes(routes)
+app.add_routes([
+    *routes
+])
 
-# import asyncio
-# from db import init_db
+import asyncio
+from db import init_db
 
-# asyncio.get_event_loop().run_until_complete(init_db())
+asyncio.get_event_loop().run_until_complete(init_db())
 
 web.run_app(app)
